8d473ca585479ea64a36d0e8a676e94bef517035
VitoUnisa <v.scarpitta3@studenti.unisa.it>
2026-05-18 16:11:07 +0200

    segnalazioni.h

--- segnalazioni.h
+++ segnalazioni.h
@@ -1,30 +1,34 @@
-//In questo file (Segnalazioni.h) sono presenti le funzioni prototipo le quali agiscono da interfaccia
-
-#ifndef SEGNALAZIONI_H //ho preferito utilizzare le macro #ifndef (if not defined) ed #endif come forma di sicurezza per il corretto funzionamento del programma
+#ifndef SEGNALAZIONI_H      //"ifndef" ed "endif" per evitare inclusioni multiple
 #define SEGNALAZIONI_H
 
+//Librerie utilizzate
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
-//Definizione della struttura per singola segnalazione
+//Struttura delle segnalazioni
 typedef struct Segnalazione {
-    int codice;                  //Codice identificativo unico
-    char nome[50];               //Nome del cittadino
-    char categoria[50];          //Categoria del problema (es. buche, illuminazione)
-    char descrizione[200];       //Descrizione dettagliata
-    char data[15];               //Data di inserimento (es. GG/MM/AAAA)
-    int urgenza;                 //Livello di urgenza (es. 1 bassa, 5 alta)
-    int stato;                   //0 = Aperta, 1 = In lavorazione, 2 = Chiusa
-    struct Segnalazione* next;   //Puntatore al nodo successivo della lista
+    int codice;             //Codice identificativo
+    char nome[50];          //Nome deò cittadino
+    char categoria[50];     //Categoria della segnalazione
+    char descrizione[200];  //Descrizione della segnalazione
+    char data[15];          //Data di inserimento
+    int urgenza;            //Livello urgenza della segnalazione
+    int stato;              //Stato della segnalazione: 0 = Aperta, 1 = In lavorazione, 2 = Chiusa
+
+    struct Segnalazione* next;  //Puntatore al nodo della lista successivo 
 } Segnalazione;
 
-//Prototipi di funzioni (Specifica Sintattica)
-Segnalazione* inserisciSegnalazione(Segnalazione* head, int* contatoreId);
-void visualizzaTutte(Segnalazione* head);
-void ricercaPerCategoria(Segnalazione* head, char* categoriaCercata);
-void aggiornaStato(Segnalazione* head, int codiceCercato, int nuovoStato);
-Segnalazione* eliminaSegnalazione(Segnalazione* head, int codiceDaEliminare);
-void generaReport(Segnalazione* head);
+//Operazioni e prototipi delle funzioni:
+Segnalazione* inserisciSegnalazione(Segnalazione* head, int* contatoreId);      //inserire nuova segnalazione
+void visualizzaTutte(Segnalazione* head);                                       //elenca tutte le segnalazioni
+void ricercaPerCategoria(Segnalazione* head, char* categoriaCercata);           //ricerca segnalazioneper categoria
+void ricercaPerCodice(Segnalazione* head, int codice);                          //ricerca segnalazione per codice
+void aggiornaStato(Segnalazione* head, int codiceCercato, int nuovoStato);      //aggiornamento dello stato di una segnalazione
+void visualizzaPerStato(Segnalazione* head, int stato);                         //visualizza le segnalazioni in un determnato stato
+void visualizzaUrgenti(Segnalazione* head);                                     //visualizza quelle più urgenti (stato >= 4)
+Segnalazione* eliminaSegnalazione(Segnalazione* head, int codiceDaEliminare);   //eliminazione di una segnalazione
+void generaReport(Segnalazione* head);                                          //generatore report delle segnalazioni
+void liberaMemoria(Segnalazione* head);                                         //liberazione della memoria 
 
 #endif
